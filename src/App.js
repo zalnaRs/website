a733dcd1f5d5497d94b21bb80299e715ad5e4419
zalnaRs <thezalnar@hotmail.com>
2020-08-16 15:59:24 +0200

    asd

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/index.css';
+import {AppBar, Toolbar, Typography} from '@material-ui/core';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( 
+        <div className = "App">
+           <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6">
+                        zalnaRs
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
 }
 
 export default App;
