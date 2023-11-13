7344e6c6955c0838c896d2b498f8612c28d83ce3
zalnaRs <zalnars@proton.me>
2023-11-13 13:43:59 +0100

    Update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,10 +3,11 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'media',
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter Variable', ...defaultTheme.fontFamily.sans],
+        sans: ['Inter Variable', 'Cantarell', ...defaultTheme.fontFamily.sans],
       },
     },
   },
