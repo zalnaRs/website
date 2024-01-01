520daf6c35928493cc43466f4e07602a3d2b9ed0
zalnaRs <zalnars@proton.me>
2024-01-01 14:31:17 +0100

    updates

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,28 @@
 
 export const SITE_TITLE = 'zalnaRs';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+export const projects = [
+  {
+    title: 'ControllerAsInput',
+    href: 'https://github.com/zalnaRs/ControllerAsInput',
+    description: 'Allows you to use a xbox controller as a mouse.',
+  },
+  {
+    title: 'Website',
+    href: '/',
+    description: 'Made with Astro, the best website in 2023.',
+  },
+  {
+    title: 'CarLinkShortcut',
+    href: '/projects/CarLinkShortcut',
+    description:
+      'An app to open Car Link 2.0 by pressing the "NAV" button on the radio.',
+  },
+  {
+    title: 'Everything Launcher',
+    href: '/projects/everythinglauncher',
+    description:
+      'Allows you to open all applications installed on your Android/Google TV.',
+  },
+];
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,5 +12,5 @@ export default {
       },
     },
   },
-  plugins: [prose],
+  plugins: [prose()],
 };
