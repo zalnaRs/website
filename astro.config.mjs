55a022eb1a529b7eff55840cb4535a27d05656fc
zalnaRs <zalnars@proton.me>
2025-06-17 22:08:04 +0200

    feat: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,16 @@ import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://zalnars.vercel.app",
   integrations: [mdx(), sitemap(), tailwind()],
+
   experimental: {
     csp: true,
   },
+
+  adapter: vercel(),
 });
